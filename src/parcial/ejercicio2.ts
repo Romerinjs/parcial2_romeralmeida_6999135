a7017940042be08d843d1a7920ea76e7815dd9e8
Romerinjs <raalmeidab22@itp.edu.co>
2024-10-23 21:23:45 -0500

    ej_2 array boleanos

--- src/parcial/ejercicio2.ts
+++ src/parcial/ejercicio2.ts
@@ -1,5 +1,12 @@
 // Realizar las siguientes operaciones utilizando arrays básicos, tuplas y manipulacion de arrays y busqueda de elementos
+// array de numeros
 const arrays: [number[]] = [[1, 2, 3, 4, 5]];
 console.log ('El array de numeros es: ', arrays)
+// array de frutas
 const fruitsName: [string[]] = [['strawberry', 'apple', 'banana', 'kiwi', 'orange']];
 console.log('El array de nombres de frutas es: ', fruitsName)
+
+// array de booleanos
+
+const boolArray: [boolean[]] = [[true, false, true]];
+console.log('El array de booleanos es: ', boolArray)
