8a779efb323c7c223a1b44d02c3d8c2ed229d97d
Romerinjs <raalmeidab22@itp.edu.co>
2024-10-23 21:22:14 -0500

    ejer_2 array de frutas e impresion

--- src/parcial/ejercicio2.ts
+++ src/parcial/ejercicio2.ts
@@ -1,3 +1,5 @@
 // Realizar las siguientes operaciones utilizando arrays básicos, tuplas y manipulacion de arrays y busqueda de elementos
 const arrays: [number[]] = [[1, 2, 3, 4, 5]];
 console.log ('El array de numeros es: ', arrays)
+const fruitsName: [string[]] = [['strawberry', 'apple', 'banana', 'kiwi', 'orange']];
+console.log('El array de nombres de frutas es: ', fruitsName)
